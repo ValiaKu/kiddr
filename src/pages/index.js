5b21b62882343efa2a06fdb2be0765b3bea1dbcf
Valentyna Kuzmych <kovalska@gmail.com>
2023-04-09 20:33:13 +0300

    test mmit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const IndexPage = () => {
   return (
     <div>
-      <h1>Lear to design and code React apps</h1>
+      <h1>Learn to design and code React apps test test test</h1>
       <p>Watch the video about Gatsby the latest commit</p>
     </div>
   );
