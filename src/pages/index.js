d228a1b52f1bd9ac5479c12d97e53d728a40c3b6
Valentyna Kuzmych <kovalska@gmail.com>
2023-04-09 20:38:11 +0300

    test with message

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import * as React from "react";
 const IndexPage = () => {
   return (
     <div>
-      <h1>Learn to design and code React apps test test test</h1>
+      <h1>
+        Learn to design and code React apps test test test fsadf asdfasdfgit
+      </h1>
       <p>Watch the video about Gatsby the latest commit</p>
     </div>
   );
