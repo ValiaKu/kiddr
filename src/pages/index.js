fad7da000509fcf786ae3ad1a32854c4baf7d3fe
Valentyna Kuzmych <kovalska@gmail.com>
2023-04-09 20:50:26 +0300

    add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import * as React from "react";
+import "./index.css";
 
 const IndexPage = () => {
   return (
     <div>
-      <h1>
-        Learn to design and code React apps test test test fsadf asdfasdfgit
-      </h1>
+      <h1>Learn to design and code React apps</h1>
       <p>Watch the video about Gatsby the latest commit</p>
     </div>
   );
